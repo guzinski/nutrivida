911b3240656c2901f287769d46f7008d4814ccf7
Luciano Guzinski <lucianoguzinski@gmail.com>
2015-04-16 17:50:43 -0300

    Inicio Symfony Base

---
+++ README.md
@@ -0,0 +1,4 @@
+nutrvida
+========
+
+A Symfony project created on April 16, 2015, 5:11 pm.
---
+++ src/Nutivida/LojaBundle/NutividaLojaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nutivida\LojaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NutividaLojaBundle extends Bundle
+{
+}
